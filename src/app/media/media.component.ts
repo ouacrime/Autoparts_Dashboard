47d3e155f13c224fb0c89c951439fa78e576b464
othmane <othmaneouacrim@gmail.com>
2024-05-07 07:08:23 +0100

    create side nav and add the commponnent i need

---
+++ src/app/media/media.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-media',
+  templateUrl: './media.component.html',
+  styleUrl: './media.component.scss'
+})
+export class MediaComponent {
+
+}
---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+    {
+        routerLink: 'dashboard',
+        icon: 'fal fa-home',
+        label: 'Dashboard'
+    },
+    {
+        routerLink: 'products',
+        icon: 'fal fa-box-open',
+        label: 'Products'
+    },
+    {
+        routerLink: 'statistics',
+        icon: 'fal fa-chart-bar',
+        label: 'Statistics'
+    },
+    {
+        routerLink: 'coupens',
+        icon: 'fal fa-tags',
+        label: 'Coupens'
+    },
+    {
+        routerLink: 'pages',
+        icon: 'fal fa-file',
+        label: 'Pages'
+    },
+    {
+        routerLink: 'media',
+        icon: 'fal fa-camera',
+        label: 'Media'
+    },
+    {
+        routerLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Settings'
+    },
+];
